b1ebd6612ad985d1b15ad6abd39f541f3405b98a
ayaanshatomic810-dev <ayaansh.atomic810@gmail.com>
2026-06-12 16:06:41 -0700

    Add files via upload

---
+++ Configuration.py
@@ -0,0 +1,48 @@
+word = ("AFPP Coding Workspace @02026. All rights reserved.")
+
+Alpha1  = ("1 = Alpha A.AFPP Movie/Records = https://www.youtube.com/@AmolAyaansh ") 
+
+Alpha2  = ("2 = Alpha B.AFPP Coding Workspace = file.Macintosh/Neo.AFPP/CodeSpace")
+
+Alpha3  = ("3 = Alpha C.AFPP Gaming/News = https://www.youtube.com/@redstudioFremontPleasanton")
+
+Web1B  = ("1B = Web Web.AFPP Website/work = https://sites.google.com/view/afp21/home")
+
+Web2B  = ("2B = Web Web.AFPP Website/wixLogin=  https://ayaanshatomic810.wixsite.com/atomic-and-hca")
+
+Web3B  = ("3B = Web Web.AFPP Website/CodeByAYAANSH =  https://sites.google.com/view/cfa-m00nhelp/home")
+
+Web4B = ("4B = Web Web.AFPP Website/AtomicWorkWix =  https://ayaanshatomic810.wixsite.com/video-collaboration")
+
+git = ("GitHub =https://github.com/ayaanshatomic810-dev")
+
+extra = (" AFPP work calculator = https://ayaanshatomic810-dev.github.io/afpp-codespace-calculator/")
+extrA2 = ("AFPP User calculator = ")
+print (word)
+
+print (Alpha1)
+
+print (Alpha2)
+
+print (Alpha3)
+
+print (Web1B)
+
+print (Web2B)
+
+print (Web3B)
+
+print (Web4B)
+
+print (git)
+# This is the configuration file for AFPP Coding Workspace. It contains important information about our projects, websites, and GitHub repository. Please refer to this file for any updates or changes related to our work.
+formula = False
+formula = ("xroot=1=Web1+Alpha1, yroot=2=Web2+Alpha2, zroot=3=Web3+Alpha3, wroot=4=Web4+git")
+# xroot=1=Web1+Alpha1, yroot=2=Web2+Alpha2, zroot=3=Web3+Alpha3, wroot=4=Web4+git
+# This variable does not have to be true and shouldn't be as it doesn't amount to anything. It is just a placeholder for any future formulas or calculations that may be needed in the future. Please do not worry about this variable as it is not relevant to our current work and is only included for potential future use.
+if formula == True:
+    print("Formula is true, but it shouldn't be. Please check the configuration file for any errors.")
+else:
+    print("Formula is false, which is expected. Please refer to the configuration file for the correct information.")
+
+print(formula)
